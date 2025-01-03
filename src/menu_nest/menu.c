089b338e14780fda0969b0d25273964ee36ca60b
Thybing <yanbinghao86@gmail.com>
2025-01-04 03:10:53 +0800

    将menu结构体放于c文件之中。对外隐藏。优化了其接口。修正注释。

--- src/menu_nest/menu.c
+++ src/menu_nest/menu.c
@@ -2,6 +2,33 @@
 #include "menu_nest/common.h"
 #include "stdbool.h"
 
+/**
+ * ************************************************************************
+ * @brief 菜单模块
+ * 
+ * @warning  请在使用前进行初始化，禁止私自创建，请采用get函数获取唯一对象
+ * ************************************************************************
+ */
+typedef struct MN_menu{
+    /// @brief 菜单主页面指针
+    MN_page * mp_main_page;
+
+    /// @brief 菜单历史页面栈
+    MN_page ** mpp_history_page_stack;
+    /// @brief 菜单历史页面栈顶下标(仅有主页面时为0)
+    uint32_t m_history_top_index;
+    /// @brief 菜单交互模块
+    MN_interaction * mp_interaction;
+
+    /// @brief 菜单输入队列
+    input_t * mp_input_queue;
+    /// @brief 菜单输入队列头
+    uint32_t input_queue_head;
+    /// @brief 菜单输入队列尾
+    uint32_t input_queue_tail;
+
+}MN_menu;
+
 
 MN_menu o_MN_menu;
 static MN_page * history_page_stack[MN_HISTORY_PAGE_MAX] = {0};
@@ -20,10 +47,6 @@ void MN_menu_init(MN_page * p_main_page){
     o_MN_menu.input_queue_tail = 0;
 
     o_MN_menu.mp_interaction = MN_interaction_create(&o_MN_menu);
-}
-
-MN_menu * MN_get_menu(){
-    return &o_MN_menu;
 }
 
 static bool MN_is_input_queue_empty(){
@@ -63,9 +86,12 @@ MN_page * MN_get_cur_page(){
 
 void MN_menu_page_forward(MN_page * const p_target_page){
     MN_assert(p_target_page);
+
     if(o_MN_menu.m_history_top_index < MN_HISTORY_PAGE_MAX - 1){
         (o_MN_menu.m_history_top_index)++;
         (o_MN_menu.mpp_history_page_stack)[(o_MN_menu.m_history_top_index)] = p_target_page;
+
+        // 调用目标页面的on_forward回调
         p_target_page->mp_on_forward(p_target_page,NULL);
     }
 }
@@ -89,13 +115,14 @@ void MN_menu_page_retreat(){
     }
 }
 
-void MN_menu_set_handle_callback(const interaction_callback_t callback){
-    MN_interaction_set_handle_callback(o_MN_menu.mp_interaction,callback);
-}
-
 void MN_menu_rendering(){
     MN_render_rendering(MN_get_cur_page()->mp_render,NULL);
 }
+
+MN_interaction * MN_menu_get_interaction(){
+    return o_MN_menu.mp_interaction;
+}
+
 
 void MN_menu_input(const input_t input){
     MN_input_enqueue(input);
